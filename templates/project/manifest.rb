b711aa624e43dda9832c8d7e5bbb2d3bf6fedcaf
Percy Lau <percy.lau@gmail.com>
2010-03-21 20:32:48 +0800

    Added grid debug

--- templates/project/manifest.rb
+++ templates/project/manifest.rb
@@ -1,2 +1,5 @@
 stylesheet 'grid.sass', :media => "screen, projection"
 stylesheet 'text.sass', :media => "screen, projection"
+stylesheet 'debug.sass', :media => "screen, projection"
+image '12_col.gif'
+image '16_col.gif'
